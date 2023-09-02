146ccc619837fabb0923353c283bd1c1052a6176
Zhen Yang <yz20030303@gmail.com>
2023-09-02 17:53:28 +0100

    Update with scan_data_plot

--- src/scan_data_plot.py
+++ src/scan_data_plot.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
 
 # Load data from CSV
-# TODO: ask for file path
-data = pd.read_csv(r'C:\Programming\Python\Funky Pendulum_local\scan_for_response_test_data\scan_data.csv')
+
+while True:
+    try:
+        file_path = input('Enter file path: ')
+        data = pd.read_csv(file_path)
+        break
+    except FileNotFoundError:
+        print('File not found. Please try again.')
 
 # Calculate response amplitude to driving amplitude ratio
 data['amp_ratio'] = data['response_amp'] / data['driving_amp']
@@ -15,14 +23,12 @@ data.sort_values(by='driving_freq', inplace=True)
 
 # Plot the ratio against driving frequency with different colors
 plt.figure(figsize = (10, 5))
-for parent_dir in parent_dirs:
-    if(parent_dir == '11-08-csv' or parent_dir == '10-08-csv'):
-        continue
+for index, parent_dir in enumerate(parent_dirs):
     subset = data[data['parent_dir'] == parent_dir]
-    # plt.plot(subset['driving_freq'], abs(subset['amp_ratio']), '--', label = parent_dir)
+    plt.plot(subset['driving_freq'], abs(subset['amp_ratio']), '--', label = parent_dir, color = colors[index])
     plt.errorbar(subset['driving_freq'], abs(subset['amp_ratio']), 
                  yerr = abs(subset['response_amp_err'] / subset['driving_amp']), 
-                 fmt = 'o', label = parent_dir)
+                 fmt = 'o', label = parent_dir, color = colors[index])
 plt.xlabel('Driving Frequency / Hz')
 plt.ylabel('Response Amp / Driving Amp')
 plt.title('Response Amplitude to Driving Amplitude Ratio vs Driving Frequency')
@@ -31,12 +37,15 @@ plt.grid(True)
 
 # Plot phase against driving frequency with different colors
 plt.figure(figsize = (10, 5))
-for parent_dir in parent_dirs:
-    if(parent_dir == '11-08-csv' or parent_dir == '10-08-csv'):
-        continue
+for index, parent_dir in enumerate(parent_dirs):
     subset = data[data['parent_dir'] == parent_dir]
-    # plt.plot(subset['driving_freq'], subset['phase'], '--', label = parent_dir)
-    plt.errorbar(subset['driving_freq'], subset['phase'], yerr = abs(subset['phase_err']), fmt = 'o', label = parent_dir)
+    plt.plot(subset['driving_freq'], subset['phase'], '--', label = parent_dir, color = colors[index])
+    plt.errorbar(subset['driving_freq'], 
+                 subset['phase'], 
+                 yerr = abs(subset['phase_err']), 
+                 fmt = 'o', 
+                 label = parent_dir, 
+                 color = colors[index])
 plt.xlabel('Driving Frequency / Hz')
 plt.ylabel('Phase / pi')
 plt.title('Phase vs Driving Frequency')
