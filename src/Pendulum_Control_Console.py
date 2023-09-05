2b8dd0c8969c562bf6f7f337261231549a1bb8bd
Zhen Yang <yz20030303@gmail.com>
2023-09-05 12:33:50 +0100

    update

--- src/Pendulum_Control_Console.py
+++ src/Pendulum_Control_Console.py
@@ -594,7 +594,8 @@ class data():
                 csvfile.close()
 
         print("\nExported to " + filename + "\n")
-        print("\nExported to " + filename_fft + "\n")
+        if(module_name != 'pid'):
+            print("\nExported to " + filename_fft + "\n")
         
     def fft_index_list(self):
         '''return the list and average time spacing'''
