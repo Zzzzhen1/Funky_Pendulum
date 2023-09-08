487ceb13678dbc7264545bc638515ed1ce2b855f
Zhen Yang <yz20030303@gmail.com>
2023-09-08 14:36:47 +0100

    update the multiphase plot

--- src/Pendulum_Control_Console.py
+++ src/Pendulum_Control_Console.py
@@ -158,9 +158,8 @@ class data():
                         self.phase_list = [(0., 0.)] * self.plot_length * (self.wait_to_stable + 1) * 10
                     else: 
                         self.phase_list = None
-                        self.multi_phase_list = [[(0., 0.)] * self.plot_length * (self.wait_to_stable + 1) * 10] * self.omega_num
                         for i in range(self.omega_num):
-                            self.multi_phase_list[i] = [(0., 0.)] * self.plot_length
+                            self.multi_phase_list[i] = [(0., 0.)] * self.plot_length* (self.wait_to_stable + 1) * 10
                     self.amp_list = [(0., 0.)] * self.plot_length * 10
                     
                 self.line_angle, = self.ax_list[0, 0].plot([], [], 'b-')
