79e06d873f9eaadb5455b29362bbe4c0f0c1fb07
Zhen Yang <yz20030303@gmail.com>
2023-09-19 10:58:48 +0100

    update with the csv writing style

--- src/auto_freq_scan.py
+++ src/auto_freq_scan.py
@@ -35,47 +35,46 @@ if(__name__ == "__main__"):
     
     cartER.path = os.getcwd()
     
+    refer_dirs = cartER.path + r'\auto_freq_scan'
+    try:
+        os.mkdir(refer_dirs)
+    except OSError:
+        pass
     # Trial run, comment this when executing the auto_scan
-    cartER.main_auto_freq_scan(
-        auto_freq = 1.5,
-        auto_amp = 100,
-        duration = 60,
-    )
-    cartER.main_auto_freq_scan(
-        auto_freq = 1.2,
-        auto_amp = 20,
-        duration = 80,
-    )
+    # cartER.main_auto_freq_scan(
+    #     auto_freq = 1.5,
+    #     auto_amp = 100,
+    #     duration = 60,
+    # )
     
     # Initialisation of the auto_scan parameters
-    # start_freq = 0.9
-    # end_freq = 1.3
-    # num_freq = 20
-    # freq_array = np.linspace(start_freq, end_freq, num_freq)
-    # start_amp = 10
-    # end_amp = 200
-    # num_amp = 20
-    # amp_array = np.linspace(start_amp, end_amp, num_amp)
-    # duration = 1.5 * fft_lengths * sampling_divs
-    # ref_csv_dir = cartER.path + r'\reference_parameters\\' + \
-    #     datetime.now().strftime("auto_refer_init-%d-%m.csv")
-    # with open(ref_csv_dir, 'w') as f:
-    #     f.write(['index' ,'start_time', 'freq', 'amp_0'])
-    #     f.close()
-    # index = 0
-    # for freq in freq_array:
-    #     for amp in amp_array:
-    #         index += 1
-    #         with open(ref_csv_dir, 'a') as f:
-    #             f.write([index, datetime.now().strftime("%d-%m-%Y %H:%M:%S"), freq, amp])
-    #             f.close()
-    #         cartER.main_auto_freq_scan(
-    #             auto_freq = freq,
-    #             auto_amp = amp,
-    #             duration = duration,
-    #             ref_csv_dir = ref_csv_dir,
-    #             index = index
-    #         )
+    start_freq = 0.9
+    end_freq = 1.3
+    num_freq = 20
+    freq_array = np.linspace(start_freq, end_freq, num_freq)
+    start_amp = 10
+    end_amp = 200
+    num_amp = 20
+    amp_array = np.linspace(start_amp, end_amp, num_amp)
+    duration = 1.8 * fft_lengths * sampling_divs
+    ref_csv_dir = refer_dirs + r'\reference_parameters-' + \
+        datetime.now().strftime("init-%d-%m.csv")
+    with open(ref_csv_dir, 'w') as f:
+        f.write(['index' ,'start_time', 'freq', 'amp_0'])
+        f.close()
+    index = 0
+    for freq in freq_array:
+        for amp in amp_array:
+            index += 1
+            with open(ref_csv_dir, 'a') as f:
+                f.write([index, datetime.now().strftime("%d-%m-%Y %H:%M:%S"), freq, amp])
+                f.close()
+            time.sleep(0.1)
+            cartER.main_auto_freq_scan(
+                auto_freq = freq,
+                auto_amp = amp,
+                duration = duration,
+            )
     
     
     # TODO: read reference parameters from a csv file and then implement cartER.main_auto_freq_scan()
